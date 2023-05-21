57f209b031190c9a918ddde2460c6a0e0ef16a44
Wenqing Zong <wenqing.zong@codethink.co.uk>
2023-05-21 16:54:41 +0100

    Added Locatable trait
    This trait calculates the 3d position of a given object. Users are
    expected to provide their own implementation to this trait for their
    own structs.

--- src/bin/example.rs
+++ src/bin/example.rs
@@ -1,3 +1,23 @@
+use octree::Locatable;
+
+pub struct Point3D {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+impl Locatable for Point3D {
+    fn get_location(&self) -> [f32; 3] {
+        [self.x, self.y, self.z]
+    }
+}
+
 fn main() {
-    dbg!(octree::add(2, 5));
+    let point = Point3D {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+    };
+
+    dbg!(point.get_location());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub trait Locatable {
+    fn get_location(&self) -> [f32; 3];
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    pub struct Point3D {
+        x: f32,
+        y: f32,
+        z: f32,
+    }
+
+    impl Locatable for Point3D {
+        fn get_location(&self) -> [f32; 3] {
+            [self.x, self.y, self.z]
+        }
+    }
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    /// Should be able to get 3D location for anything implements Locatable trait.
+    fn location_trait() {
+        let point = Point3D {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        };
+
+        assert_eq!(point.get_location(), [0.0, 0.0, 0.0]);
     }
 }
