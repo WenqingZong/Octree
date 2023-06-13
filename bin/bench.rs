680b0345815f6bdeed8d636ec78c8f917dbe5ad8
Wenqing Zong <wenqing.zong98@gmail.com>
2023-06-13 14:34:13 +0100

    baseline added for comparision
    There is no change in baseline?

--- bin/bench.rs
+++ bin/bench.rs
@@ -1,14 +1,42 @@
 #[macro_use]
 extern crate timeit;
 
+use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::BTreeMap;
+use std::vec::Vec;
 
 use gnuplot::{AxesCommon, Caption, Figure};
+use other_octree::Octree as OtherOctree;
 
 use octree::point::Point3D;
 use octree::Octree;
+
+// To satisfy the other crate's requirement.
+struct Point3DIterator {
+    points: Vec<Point3D>,
+}
+
+struct Wapper(Vec<Point3D>);
+
+impl IntoIterator for Wapper {
+    type Item = [f64; 3];
+    type IntoIter = Point3DIterator;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Point3DIterator { points: self.0 }
+    }
+}
+
+impl Iterator for Point3DIterator {
+    type Item = [f64; 3];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.points
+            .pop()
+            .map(|p| [p.x as f64, p.y as f64, p.z as f64])
+    }
+}
 
 fn read_points(file: File) -> Vec<Point3D> {
     let reader = BufReader::new(file);
@@ -29,9 +57,25 @@ fn read_points(file: File) -> Vec<Point3D> {
 fn bench_test(points: &Vec<Point3D>) -> BTreeMap<usize, f64> {
     let mut results = BTreeMap::new();
     for i in (0..=points.len()).step_by(1000) {
-        results.insert(i, timeit_loops!(10, {
-            Octree::new(points.iter().take(i).collect());
-        }));
+        results.insert(
+            i,
+            timeit_loops!(10, {
+                Octree::new(points.iter().take(i).collect());
+            }),
+        );
+    }
+    results
+}
+
+fn bench_test_baseline(points: &Vec<Point3D>) -> BTreeMap<usize, f64> {
+    let mut results = BTreeMap::new();
+    for i in (0..points.len()).step_by(1000) {
+        results.insert(
+            i,
+            timeit_loops!(10, {
+                OtherOctree::new(Wapper(points.clone()));
+            }),
+        );
     }
     results
 }
@@ -42,19 +86,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = File::open("./data/points.txt")?;
     let points = read_points(file);
     let results = bench_test(&points);
+    let baseline_results = bench_test_baseline(&points);
     let output_path = "./data/bench.png";
 
     let mut figure = Figure::new();
     let num_points: Vec<usize> = results.keys().cloned().collect();
     let durations: Vec<f64> = results.values().cloned().collect();
+    let durations_baseline: Vec<f64> = baseline_results.values().cloned().collect();
 
-    figure.axes2d()
-    .lines_points(&num_points, &durations, &[Caption("My Octree, Single Thread")])
-    .set_title("Octree Building Benchmark", &[])
-    .set_x_label("Number of Points", &[])
-    .set_y_label("Durations (sec)", &[]);
+    figure
+        .axes2d()
+        .lines_points(
+            &num_points,
+            &durations,
+            &[Caption("My Octree, Single Thread")],
+        )
+        .lines_points(&num_points, &durations_baseline, &[Caption("Baseline")])
+        .set_title("Octree Building Benchmark", &[])
+        .set_x_label("Number of Points", &[])
+        .set_y_label("Durations (sec)", &[]);
 
-    figure.save_to_png(output_path, 800, 600).expect("Failed to save figure");
+    figure
+        .save_to_png(output_path, 800, 600)
+        .expect("Failed to save figure");
 
     Ok(())
 }
